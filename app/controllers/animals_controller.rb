47499f69b7ecdc2207c65759c99a6b8aad744f89
Jorge Bejar <jorge@wyeworks.com>
2015-06-16 12:07:30 -0300

    Pure ruby class implementation

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,14 @@
 class AnimalsController < ApplicationController
   def index
+    path = params[:path] || ''
+    animals = Animal.find(path)
+
+    @current_path = 'animals/' + path
+
+    @current = animals[:current]
+    @children = animals[:children]
+
+    @parent = path.split('/')[-2] || 'Main Menu'
+    @parent_path = 'animals/' + path.split('/')[0..-2].join('/')
   end
 end
